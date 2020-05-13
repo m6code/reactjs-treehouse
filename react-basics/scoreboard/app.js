71ae5f0a4faa01a253ea3f3e1f59ce544aef7d07
M6code <benjamynfrank101@gmail.com>
2020-05-13 10:54:31 +0100

    Components as arrow functions

--- react-basics/scoreboard/app.js
+++ react-basics/scoreboard/app.js
@@ -1,15 +1,13 @@
-function Header() {
+const Header = () => {
     // React component are required to begin with capital letter to differential between html tags and react component JSX tags.
-    return (
-        <header>
-            <h1>Scorebord</h1>
-            <span className="stats">Players: 1</span>
-        </header>
-
+    return (<header>
+        <h1>Scorebord</h1>
+        <span className="stats">Players: 1</span>
+    </header>
     );
 }
 
 ReactDOM.render(
-    <Header/>,
+    <Header />,
     document.getElementById('root')
 );

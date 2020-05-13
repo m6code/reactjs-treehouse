a907303a8e0a457fa8185dde16bd08d5c2416487
M6code <benjamynfrank101@gmail.com>
2020-05-13 10:51:49 +0100

    Creating and rendering components

--- react-basics/scoreboard/app.js
+++ react-basics/scoreboard/app.js
@@ -1,18 +1,15 @@
-const name = "Ben";
+function Header() {
+    // React component are required to begin with capital letter to differential between html tags and react component JSX tags.
+    return (
+        <header>
+            <h1>Scorebord</h1>
+            <span className="stats">Players: 1</span>
+        </header>
 
-const desc = 'I just learned how to create a React node and render it into the DOM';
-
-const myTitleID = 'main-title';
-
-const header =(
-    <header>
-        <h1 id={myTitleID}>{name}'s First React App</h1>
-        <p className="main-desc">{desc}</p>
-        {/* comments in jsx like this */}
-    </header>
-);
+    );
+}
 
 ReactDOM.render(
-    header,
+    <Header/>,
     document.getElementById('root')
 );

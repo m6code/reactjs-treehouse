57923450e67db420c39d25b6ff7622d6e832709a
M6code <benjamynfrank101@gmail.com>
2020-05-13 11:25:00 +0100

    restructure component

--- react-basics/scoreboard/app.js
+++ react-basics/scoreboard/app.js
@@ -31,8 +31,18 @@ const Player = () => {
     );
 }
 
+const App = () => {
+    return (
+        <div className="scoreboard">
+            <Header />
+
+            {/* Players list */}
+            <Player />
+        </div>
+    );
+}
 
 ReactDOM.render(
-    <Player />,
+    <App />,
     document.getElementById('root')
 );

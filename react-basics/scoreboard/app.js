77e881823f8da4c1036620b0390161997046b5c2
M6code <benjamynfrank101@gmail.com>
2020-05-13 11:43:08 +0100

    Go offline with the react, react-dom and babel

--- react-basics/scoreboard/app.js
+++ react-basics/scoreboard/app.js
@@ -1,8 +1,8 @@
-const Header = () => {
+const Header = (props) => {
     // React component are required to begin with capital letter to differential between html tags and react component JSX tags.
     return (<header>
-        <h1>Scorebord</h1>
-        <span className="stats">Players: 1</span>
+        <h1>{props.title}</h1>
+        <span className="stats">Players: {props.totalPlayers}</span>
     </header>
     );
 }
@@ -31,10 +31,11 @@ const Player = () => {
     );
 }
 
+// App component
 const App = () => {
     return (
         <div className="scoreboard">
-            <Header />
+            <Header title="Scoreboard" totalPlayers={1} />
 
             {/* Players list */}
             <Player />

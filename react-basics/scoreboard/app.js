422a51093bcac13c955ae4f5f3bb30b88cbfcfbe
M6code <benjamynfrank101@gmail.com>
2020-05-13 11:01:12 +0100

    create the Player component

--- react-basics/scoreboard/app.js
+++ react-basics/scoreboard/app.js
@@ -7,7 +7,24 @@ const Header = () => {
     );
 }
 
+// Player component
+const Player = () => {
+    return (
+        <div className="player">
+            <span className="player-name">
+                Ben
+            </span>
+            <span className="counter">
+                <button className="counter-action decrement"> - </button>
+                <span className="counter-score">34</span>
+                <button className="counter-action increment"> + </button>
+            </span>
+        </div>
+    );
+}
+
+
 ReactDOM.render(
-    <Header />,
+    <Player />,
     document.getElementById('root')
 );

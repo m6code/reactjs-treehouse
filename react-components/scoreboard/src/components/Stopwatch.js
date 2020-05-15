f5990c56b6dd7835c72c46d3a6abe1a95ac450d2
M6code <benjamynfrank101@gmail.com>
2020-05-15 08:42:07 +0100

    Reset timer and prevent memory leaks with componentWillUnmount lifecycle method

--- react-components/scoreboard/src/components/Stopwatch.js
+++ react-components/scoreboard/src/components/Stopwatch.js
@@ -14,6 +14,10 @@ export class Stopwatch extends Component {
         this.intervalID = setInterval(() => this.tick(), 100);
     }
 
+    componentWillUnmount(){
+        clearInterval(this.intervalID);
+    }
+
     tick = () => {
         if (this.state.isRunning) {
             const now = Date.now();
@@ -30,21 +34,30 @@ export class Stopwatch extends Component {
         }));
 
         if (!this.state.isRunning) {
-            this.setState({ 
-                previousTime: Date.now(), 
+            this.setState({
+                previousTime: Date.now(),
             });
         }
     }
 
+    handleReset = () => {
+        this.setState({
+            elapsedTime: 0
+        });
+    }
+
     render() {
+        const seconds = Math.floor(this.state.elapsedTime / 1000)
         return (
             <div className="stopwatch">
                 <h2>Stopwatch</h2>
-                <span className="stopwatch-time">0</span>
+                <span className="stopwatch-time">
+                    {seconds}
+                </span>
                 <button onClick={this.handleStopwatch}>
                     {this.state.isRunning ? "Stop" : "Start"}
                 </button>
-                <button>Reset</button>
+                <button onClick={this.handleReset}>Reset</button>
             </div>
         );
     }

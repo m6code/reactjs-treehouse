479022f7cb1e965f08d2ffb635440681e50772fe
M6code <benjamynfrank101@gmail.com>
2020-05-15 08:09:27 +0100

    Add Stopwatch component

--- react-components/scoreboard/src/components/Header.js
+++ react-components/scoreboard/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Stats from './Stats'
+import Stopwatch from './Stopwatch'
 
 const Header = (props) => {
   // React component are required to begin with capital letter to differential between html tags and react component JSX tags.
@@ -8,6 +9,7 @@ const Header = (props) => {
     <header>
       <Stats players={props.players} />
       <h1>{props.title}</h1>
+      <Stopwatch />
     </header>
   );
 }

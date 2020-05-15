e6e11170ca12d76268000d1084a7ae33b788ae45
M6code <benjamynfrank101@gmail.com>
2020-05-15 09:50:37 +0100

    Refs in funtional component

--- react-components/scoreboard/src/components/AddPlayerForm.js
+++ react-components/scoreboard/src/components/AddPlayerForm.js
@@ -1,30 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 
 
 const AddPlayerForm = ({ addPlayer }) => {
 
-  let playerInput = React.createRef();
-  let handleSubmit = (e) => {
-    e.preventDefault();
-    addPlayer(playerInput.current.value);
-    e.currentTarget.reset();
-  }
+    let playerInput = React.createRef();
+    let handleSubmit = (e) => {
+        e.preventDefault();
+        addPlayer(playerInput.current.value);
+        e.currentTarget.reset();
+    }
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input 
-        type="text" 
-        ref={playerInput}
-        placeholder="Enter a player's name"
-      />
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                ref={playerInput}
+                placeholder="Enter a player's name"
+            />
 
-      <input 
-        type="submit" 
-        value="Add Player" 
-      />
-    </form>
-  ); 
+            <input
+                type="submit"
+                value="Add Player"
+            />
+        </form>
+    );
 }
 
 

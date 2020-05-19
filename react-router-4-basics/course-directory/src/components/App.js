2a520933d7bb267211432630e01592d163f86715
M6code <benjamynfrank101@gmail.com>
2020-05-19 18:48:43 +0100

    Using URL Parameters

--- react-router-4-basics/course-directory/src/components/App.js
+++ react-router-4-basics/course-directory/src/components/App.js
@@ -12,6 +12,7 @@ import About from './About';
 import Teachers from './Teachers';
 import Courses from './Courses';
 import NotFound from './NotFound';
+import Featured from './Featured';
 
 const App = () => (
   <BrowserRouter>
@@ -21,7 +22,8 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/about" render={ () => <About title='About' /> } />
-        <Route path="/teachers" component={Teachers} />
+        <Route exact path="/teachers" component={Teachers} />
+        <Route path="/teachers/:topic/:fname-:lname" component={Featured} />
         <Route path="/courses" component={Courses} />
         <Route component={NotFound} />
       </Switch>

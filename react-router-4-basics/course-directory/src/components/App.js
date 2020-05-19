7812b992c12e0f5e84637516e91a39d06a618a5e
M6code <benjamynfrank101@gmail.com>
2020-05-19 18:33:16 +0100

    Displaying a 404 page with react-router-dom

--- react-router-4-basics/course-directory/src/components/App.js
+++ react-router-4-basics/course-directory/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // App components
 import Header from './Header';
@@ -7,15 +7,19 @@ import Home from './Home';
 import About from './About';
 import Teachers from './Teachers';
 import Courses from './Courses';
+import NotFound from './NotFound';
 
 const App = () => (
   <BrowserRouter>
     <div className="container">
       <Header />
+      <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/about" render={() => <About />} />
       <Route path='/teachers' component={Teachers} />
       <Route path='/courses' component={Courses} />
+      <Route component={NotFound}/>
+      </Switch>
     </div>
   </BrowserRouter>
 );

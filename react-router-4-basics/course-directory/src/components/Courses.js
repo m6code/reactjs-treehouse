6f85546a06a49c21657fdc0f3d1b13ff8a46c45a
M6code <benjamynfrank101@gmail.com>
2020-05-19 17:54:20 +0100

    Complete Routing Challenge

--- react-router-4-basics/course-directory/src/components/Courses.js
+++ react-router-4-basics/course-directory/src/components/Courses.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { BrowserRoute, Route, NavLink } from 'react-router-dom';
+import CSS from './courses/CSS';
+import HTML from './courses/HTML';
+import JavaScript from './courses/JavaScript';
 
 const Courses = () => (
   <div className="main-content courses">
     <div className="course-header group">
-      <h2>Courses</h2> 
+      <h2>Courses</h2>
       <ul className="course-nav">
-        <li><a href='/courses/html'>HTML</a></li>
-        <li><a href='/courses/css'>CSS</a></li>
-        <li><a href='/courses/javascript'>JavaScript</a></li>
+        <li><NavLink to='/courses/html'>HTML</NavLink></li>
+        <li><NavLink to='/courses/css'>CSS</NavLink></li>
+        <li><NavLink to='/courses/javascript'>JavaScript</NavLink></li>
       </ul>
     </div>
-    
+
     {/* Write routes here... */}
+    <Route path='/courses/html' component={HTML} />
+    <Route path='/courses/css' component={CSS} />
+    <Route path='/courses/javascript' component={JavaScript} />
   </div>
 );
 

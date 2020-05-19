cec92f327d1e2ff0d8c15e6a4e02d5d1426253d9
M6code <benjamynfrank101@gmail.com>
2020-05-19 17:04:12 +0100

    Navigating between Routes with Link

--- react-router-4-basics/course-directory/src/components/Header.js
+++ react-router-4-basics/course-directory/src/components/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import {Link } from 'react-router-dom';
 
 const Header = () => (
   <header>
     <span className="icn-logo"><i className="material-icons">code</i></span>
     <ul className="main-nav">
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Teachers</a></li>
-      <li><a href="#">Courses</a></li>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/about">About</Link></li>
+      <li><Link to="/teachers">Teachers</Link></li>
+      <li><Link to="/courses">Courses</Link></li>
     </ul>    
   </header>
 );

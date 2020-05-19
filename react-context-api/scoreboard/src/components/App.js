e000e8f5b57e05f678c4e0846927ceec9d31da71
M6code <benjamynfrank101@gmail.com>
2020-05-19 10:01:52 +0100

    Refactor the Provider

--- react-context-api/scoreboard/src/components/App.js
+++ react-context-api/scoreboard/src/components/App.js
@@ -1,85 +1,16 @@
-import React, { Component } from 'react';
-import { Provider } from './Context';
+import React from 'react';
 import Header from './Header';
 import PlayerList from './PlayerList';
 import AddPlayerForm from './AddPlayerForm';
 
-class App extends Component {
-  state = {
-    players: [
-      {
-        name: "Guil",
-        score: 0,
-        id: 1
-      },
-      {
-        name: "Treasure",
-        score: 0,
-        id: 2
-      },
-      {
-        name: "Ashley",
-        score: 0,
-        id: 3
-      },
-      {
-        name: "James",
-        score: 0,
-        id: 4
-      }
-    ]
-  };
-
-  // player id counter
-  prevPlayerId = 4;
-
-  handleScoreChange = (index, delta) => {
-    this.setState( prevState => ({
-      score: prevState.players[index].score += delta
-    }));
-  }
-
-  handleAddPlayer = (name) => {
-    this.setState( prevState => {
-      return {
-        players: [
-          ...prevState.players,
-          {
-            name,
-            score: 0,
-            id: this.prevPlayerId += 1
-          }
-        ]
-      };
-    });
-  }
-
-  handleRemovePlayer = (id) => {
-    this.setState( prevState => {
-      return {
-        players: prevState.players.filter(p => p.id !== id)
-      };
-    });
-  }
-
-  render() {
-    return (
-      <Provider value={{
-        players: this.state.players,
-        actions: {
-          changeScore: this.handleScoreChange,
-          removePlayer: this.handleRemovePlayer,
-          addPlayer: this.handleAddPlayer
-        }
-      }}>
-        <div className="scoreboard">
-          <Header />
-          <PlayerList />   
-          <AddPlayerForm />
-        </div>
-      </Provider>
-    );
-  }
+const App = () => {
+  return (
+    <div className="scoreboard">
+      <Header />
+      <PlayerList />   
+      <AddPlayerForm />
+    </div>
+  );
 }
 
 export default App;

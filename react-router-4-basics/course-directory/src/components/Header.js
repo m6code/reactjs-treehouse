185cc6345abd9fd8a7da7300eb48693db7ff575e
M6code <benjamynfrank101@gmail.com>
2020-05-19 17:13:41 +0100

    Styling active links

--- react-router-4-basics/course-directory/src/components/Header.js
+++ react-router-4-basics/course-directory/src/components/Header.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import {Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => (
   <header>
     <span className="icn-logo"><i className="material-icons">code</i></span>
     <ul className="main-nav">
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/about">About</Link></li>
-      <li><Link to="/teachers">Teachers</Link></li>
-      <li><Link to="/courses">Courses</Link></li>
-    </ul>    
+      {/* <li><NavLink exact to="/" activeStyle={{ background: 'tomato'}}>Home</NavLink></li>
+      <li><NavLink to="/about" activeClassName="myActiveClassname">About</NavLink></li> */}
+      <li><NavLink exact to="/">Home</NavLink></li>
+      <li><NavLink to="/about">About</NavLink></li>
+      <li><NavLink to="/teachers">Teachers</NavLink></li>
+      <li><NavLink to="/courses">Courses</NavLink></li>
+    </ul>
   </header>
 );
 

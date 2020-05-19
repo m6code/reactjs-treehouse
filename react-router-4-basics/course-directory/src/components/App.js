2e339f21822498c351d3defe017fbd5ae89fe169
M6code <benjamynfrank101@gmail.com>
2020-05-19 16:48:03 +0100

    Declaring routes

--- react-router-4-basics/course-directory/src/components/App.js
+++ react-router-4-basics/course-directory/src/components/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+// App components
+import Header from './Header';
+import Home from './Home';
+import About from './About';
+import Teachers from './Teachers';
+import Courses from './Courses';
 
 const App = () => (
-  <div className="container">
-
-  </div>
+  <BrowserRouter>
+    <div className="container">
+      <Header />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path='/teachers' component={Teachers} />
+      <Route path='/courses' component={Courses} />
+    </div>
+  </BrowserRouter>
 );
 
 export default App;

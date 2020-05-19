928caa1215fce60f6702eb3356971c545d35e330
M6code <benjamynfrank101@gmail.com>
2020-05-19 18:05:52 +0100

    Redirecting Routes

--- react-router-4-basics/course-directory/src/components/Courses.js
+++ react-router-4-basics/course-directory/src/components/Courses.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRoute, Route, NavLink } from 'react-router-dom';
+import { Redirect, Route, NavLink } from 'react-router-dom';
 import CSS from './courses/CSS';
 import HTML from './courses/HTML';
 import JavaScript from './courses/JavaScript';
@@ -16,6 +16,7 @@ const Courses = () => (
     </div>
 
     {/* Write routes here... */}
+    <Route exact path="/courses" render={() => <Redirect to='/courses/html' />} />
     <Route path='/courses/html' component={HTML} />
     <Route path='/courses/css' component={CSS} />
     <Route path='/courses/javascript' component={JavaScript} />

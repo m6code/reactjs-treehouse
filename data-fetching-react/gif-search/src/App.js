d78f61d6da8b0c440b47ec70afae4cb7280991cb
M6code <benjamynfrank101@gmail.com>
2020-05-20 09:23:59 +0100

    Fectching data with the Fetch API

--- data-fetching-react/gif-search/src/App.js
+++ data-fetching-react/gif-search/src/App.js
@@ -4,20 +4,37 @@ import SearchForm from './Components/SearchForm';
 import GifList from './Components/GifList';
 
 export default class App extends Component {
-  
+
   constructor() {
     super();
-  } 
 
-  render() { 
+    this.state = {
+      gifs: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+      .then(res => res.json())
+      .then(resData => {
+        this.setState({gifs: resData.data});
+      })
+      .catch(err => {
+        console.log('Error fetching and parsing data', err)
+      });
+  }
+
+
+  render() {
+    console.log(this.state.gifs);
     return (
       <div>
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm />      
-          </div>   
-        </div>    
+            <SearchForm />
+          </div>
+        </div>
         <div className="main-content">
           <GifList />
         </div>

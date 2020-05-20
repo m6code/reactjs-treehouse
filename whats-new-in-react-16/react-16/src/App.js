c1863722825c5fd7060cc22ffad4c3ae8f602cac
M6code <benjamynfrank101@gmail.com>
2020-05-20 08:05:29 +0100

    catching errors with componentDidCatch lifeCycle hooks

--- whats-new-in-react-16/react-16/src/App.js
+++ whats-new-in-react-16/react-16/src/App.js
@@ -5,18 +5,30 @@ import './App.css';
 import StudentForm from './components/StudentForm';
 
 class App extends Component {
-  
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
 
-        <StudentForm />
-      </div>
-    );
+  state = {
+    hasError: false
+  };
+
+  componentDidCatch() {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h3>Something went wrong and it's not my fault!</h3>
+    } else {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+
+          <StudentForm />
+        </div>
+      );
+    }
   }
 }
 

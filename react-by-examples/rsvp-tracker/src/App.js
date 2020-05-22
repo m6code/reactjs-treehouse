d323e4766078372d9547f6a5372fede0638666c2
M6code <benjamynfrank101@gmail.com>
2020-05-22 18:57:15 +0100

    write and connect handler to toggle guest confirmation

--- react-by-examples/rsvp-tracker/src/App.js
+++ react-by-examples/rsvp-tracker/src/App.js
@@ -21,6 +21,20 @@ class App extends React.Component {
     ]
   }
 
+  toggleConfirmationAt = (indexToChange) => {
+    this.setState({
+      guests: this.state.guests.map((guest, index) => {
+        if (index === indexToChange) {
+          return {
+            ...guest,
+            isConfirmed : !guest.isConfirmed
+          }
+        }
+        return guest;
+      })
+    });
+  }
+
   getTotalInvited = () => this.state.guests.length;
   // getAttendingGuests = () =>
   // getUnconfirmedGuests = () =>
@@ -59,7 +73,10 @@ class App extends React.Component {
               </tr>
             </tbody>
           </table>
-          <GuestList guests={this.state.guests}/>
+          <GuestList
+            guests={this.state.guests}
+            toggleConfirmationAt={this.toggleConfirmationAt}
+          />
         </div>
       </div>
     );
